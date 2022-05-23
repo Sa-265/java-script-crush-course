7c3f9f45744de909647dbde765060ba8b9f9f01c
Paul Mtali <paul.mtali@techie.com>
2022-05-23 15:45:58 +0200

    fixes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
 
-   //e.preventDefualt();
+   e.preventDefault();
    if(nameInput.value=== '' || emailInput.value === '') {
       msg.classList.add('error');
       msg.innerHTML ='Please enter all fields';
@@ -17,7 +17,7 @@ function onSubmit(e) {
    } else {
       const li = document.createElement('li');
       li.appendChild(document.createTextNode
-         ('${nameInput.value} : ${emailInput.value}'));
+         (`${nameInput.value} : ${emailInput.value}`));
 
          userList.appendChild(li);
          //clear fields
